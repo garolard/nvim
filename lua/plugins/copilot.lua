511083b3beba8305ecd605a110f9230b5226a1b6
Gabriel Ferreiro <gabriel.ferreiro@plainconcepts.com>
2025-04-14 08:00:27 +0200

    Added copilot plugin config

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+-- GitHub Copilot configuration
+
+return {
+  "github/copilot.vim",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<C-J>", "copilot#Accept()", { expr = true, silent = true })
+    vim.g.copilot_filetypes = {}
+  end,
+}
