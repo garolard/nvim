838670094d2c9a2fc181b7aad54008ec593351fe
Gabriel Ferreiro <gbril9119@gmail.com>
2025-04-15 09:15:20 +0200

    Updated dart lsp plugin config with flutter-tools

--- lua/plugins/dartls.lua
+++ lua/plugins/dartls.lua
@@ -1,10 +1,24 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        dartls = {},
-      },
-    },
+  "nvim-flutter/flutter-tools.nvim",
+  ft = { "dart" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
   },
+  config = function()
+    require("flutter-tools").setup({
+      decorations = {
+        app_version = true,
+      },
+      lsp = {
+        color = {
+          enabled = true,
+        },
+        settings = {
+          enableSnippets = true,
+          updateImportsOnRename = true,
+        },
+      },
+    })
+  end,
 }
