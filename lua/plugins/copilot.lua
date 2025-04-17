e5bf8d5ba79184bf0299a94d66c9f41dc9895336
Gabriel Ferreiro <gbril9119@gmail.com>
2025-04-17 18:52:20 +0200

    Ignored copilot plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,7 @@
 
 return {
   "github/copilot.vim",
+  ft = "cs",
   config = function()
     vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap("i", "<C-J>", "copilot#Accept()", { expr = true, silent = true })
