829f349ebc2f4f32113624a8b39fb687d078b61d
Gabriel Ferreiro <gbril9119@gmail.com>
2026-06-23 18:45:30 +0200

    fix: fixed setup for C#/Roslyn

--- lua/plugins/dartls.lua
+++ lua/plugins/dartls.lua
@@ -1,36 +1,55 @@
 return {
-  "nvim-flutter/flutter-tools.nvim",
-  ft = { "dart" },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "stevearc/dressing.nvim", -- optional for vim.ui.select
-  },
-  opts = {
-    decorations = {
-      statusline = {
-        app_version = true,
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    ft = { "dart" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    opts = {
+      decorations = {
+        statusline = {
+          app_version = true,
+        },
+      },
+      widget_guides = { enabled = true },
+      lsp = {
+        color = {
+          enabled = true,
+        },
+        settings = {
+          enableSnippets = true,
+          updateImportsOnRename = true,
+          completeFunctionCalls = true,
+          enableSdkFormatter = true,
+        },
       },
     },
-    widget_guides = { enabled = true },
-    lsp = {
-      color = {
-        enabled = true,
-      },
-      settings = {
-        enableSnippets = true,
-        updateImportsOnRename = true,
-        completeFunctionCalls = true,
-        enableSdkFormatter = true,
+    config = true,
+    keys = {
+      { "<leader>mfr", "<cmd>FlutterRun<cr>", desc = "Flutter Run" },
+      { "<leader>mfd", "<cmd>FlutterDevices<cr>", desc = "Flutter Devices" },
+      { "<leader>mfl", "<cmd>FlutterReload<cr>", desc = "Flutter Hot Reload" },
+      { "<leader>mfs", "<cmd>FlutterRestart<cr>", desc = "Flutter Hot Restart" },
+      { "<leader>mft", "<cmd>FlutterLogToggle<cr>", desc = "Flutter Log Toggle" },
+      { "<leader>mfq", "<cmd>FlutterQuit<cr>", desc = "Flutter Quit" },
+    },
+  },
+  -- prevent lspconfig from also attaching dartls (flutter-tools owns it)
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        dartls = function()
+          return true
+        end,
       },
     },
   },
-  config = true,
-  keys = {
-    { "<leader>mfr", "<cmd>FlutterRun<cr>", desc = "Fluter Run" },
-    { "<leader>mfd", "<cmd>FlutterDevices<cr>", desc = "Flutter Devices" },
-    { "<leader>mfl", "<cmd>FlutterReload<cr>", desc = "Flutter Hot Reload" },
-    { "<leader>mfs", "<cmd>FlutterRestart<cr>", desc = "Flutter Hot Restart" },
-    { "<leader>mft", "<cmd>FlutterLogToggle<cr>", desc = "Flutter Log Toggle" },
-    { "<leader>mfq", "<cmd>FlutterQuit<cr>", desc = "Flutter Quit" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "dart" })
+    end,
   },
 }
--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -1,9 +1,41 @@
 return {
-  "seblyng/roslyn.nvim",
-  ft = "cs",
-  ---@module 'roslyn.config'
-  ---@type RoslynNvimConfig
-  opts = {
-    filewatching = "roslyn",
+  {
+    "seblyng/roslyn.nvim",
+    ft = "cs",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      filewatching = "roslyn",
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "c_sharp" })
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = { "csharpier" },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        cs = { "csharpier" },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        omnisharp = function()
+          return true -- roslyn handles C#
+        end,
+      },
+    },
   },
 }
